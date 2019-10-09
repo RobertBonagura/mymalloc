2cf4e820b3049118102e3abed7cdebae232bdbca
Robert J. Bonagura <rjb348@butter.cs.rutgers.edu>
2019-10-09 13:44:48 -0400

    New mymalloc.c

--- test.c
+++ test.c
@@ -5,12 +5,7 @@
 int main (int argc, char *argv[])
 {
 	char* ptr1 = (char*) malloc(sizeof(char) * 89);
-	char* ptr2 = (char*) malloc(sizeof(char) * 39);
-	//char* ptr3 = (char*) malloc(sizeof(char) * 89);
-	//char* ptr4 = (char*) malloc(sizeof(char) * 89);
-	
-	showMeta((metadata*)ptr1);
-	
+	char* ptr2 = (char*) malloc(sizeof(char) * 39);	
 	return 0;
 }
 
