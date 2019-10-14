e8b11e42b6938cd75b475e863e94124502493761
Peter E. Tilton <pet27@python.cs.rutgers.edu>
2019-10-13 22:32:38 -0400

    Edits to test.c, mymalloc.c

--- test.c
+++ test.c
@@ -2,15 +2,32 @@
 #include <stdlib.h>
 #include "mymalloc.h"
 
+const int N_TESTS = 10;
+const int LOCAL_RAND_MAX = 64;
+
 int main (int argc, char *argv[])
 {
-	char* ptr1 = (char*) malloc(sizeof(char) * 89);
-	char* ptr2 = (char*) malloc(sizeof(char) * 89);
-	char* ptr3 = (char*) malloc(sizeof(char) * 89);
-	char* ptr4 = (char*) malloc(sizeof(char) * 89);
+
+        printf("\nSTARTING MALLOC:\n\n");
+
+	char* ptr1 = malloc(sizeof(char) * (rand() % LOCAL_RAND_MAX));
+        char* ptr2 = malloc(sizeof(char) * (rand() % LOCAL_RAND_MAX));
+        char* ptr3 = malloc(sizeof(char) * (rand() % LOCAL_RAND_MAX));
+        char* ptr4 = malloc(sizeof(char) * (rand() % LOCAL_RAND_MAX));
+        char* ptr5 = malloc(sizeof(char) * (rand() % LOCAL_RAND_MAX));
+        char* ptr6 = malloc(sizeof(char) * (rand() % LOCAL_RAND_MAX));
+
 	
 	showMeta((metadata*)ptr1);
 	
+	printf("\nSTARTING FREE:\n\n");
+
+	free(ptr2);
+        free(ptr3);
+        free(ptr6);
+
+        showMeta((metadata*)ptr1);
+
 	return 0;
 }
 
